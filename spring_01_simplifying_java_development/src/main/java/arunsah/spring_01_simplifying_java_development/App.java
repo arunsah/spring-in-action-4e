2dbaa31de2a660084fb3b75a26d84168fd847399
arunsah <arunkumar626@gmail.com>
2019-09-12 20:53:50 +0530

    init project

---
+++ spring_01_simplifying_java_development/src/main/java/arunsah/spring_01_simplifying_java_development/App.java
@@ -0,0 +1,13 @@
+package arunsah.spring_01_simplifying_java_development;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
